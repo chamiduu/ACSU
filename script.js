2c06f04964c0fb26559db2f3e227880ec02ce107
chamindujs <chamindujs@gmail.com>
2022-06-26 11:28:39 +0530

    Add hero animations (mouseover)

--- script.js
+++ script.js
@@ -1,3 +1,22 @@
+const butterfly = document.querySelector('.butterfly');
+const astronout = document.querySelector('.astronout');
+const heroWrapper = document.querySelector('.heroWrapper');
+
+heroWrapper.addEventListener('mousemove', function(e) {
+    let x = e.clientX / 100;
+    let y = e.clientY / 100;
+    butterfly.style.transform = `translate(${x}%, ${y}%)`;
+    astronout.style.transform = `translate(${x}%, ${y}%)`;
+})
+
+
+
+
+
+
+
+
+
 AOS.init({
     duration: 1000,
     delay: 200,
